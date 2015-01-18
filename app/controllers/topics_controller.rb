f6529b08e7cf1a3727ca0ec58dd8876ae1346069
Sean Mikalson <sean.mikalson@gmail.com>
2015-01-18 14:53:46 -0700

    Move tree creation into tree node

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -18,37 +18,7 @@ class TopicsController < ApplicationController
 	def getAllRelatedTopics (topic, depth)
 
 		rootNode = TreeNode.new(topic)
-		expandNode(rootNode, depth)
+		rootNode.expand(depth)
 		rootNode
-
-	end
-
-	def expandNode(node, level)
-		if(level > 0)
-			children = getRelatedTo(node.value)
-			children.each do |relatedTopic|
-				childNode = TreeNode.new(relatedTopic['name'])
-				
-				expandNode(childNode, level-1)
-				node.addChild(childNode)
-			end
-		end
-		node
-	end
-
-	def getRelatedTo(topic)
-		results = []
-		broadrequest = BroadFreebaseRequest.new(topic)
-		rawResponse = broadrequest.send().body
-		jsonResponse = JSON.parse(rawResponse)
-		results = jsonResponse['result']
-
-		narrowrequest = NarrowFreebaseRequest.new(topic)
-		rawResponse = narrowrequest.send().body
-		jsonResponse = JSON.parse(rawResponse)
-		jsonResponse['result'].each do |result|
-			results.push(result)
-		end
-		results
 	end
 end
--- app/models/treenode.rb
+++ app/models/treenode.rb
@@ -10,4 +10,32 @@ class TreeNode
 		@children.push(node)
 	end
 
+	def expand(level)
+		if(level > 0)
+			children = getRelatedTo(@value)
+			children.each do |relatedTopic|
+				childNode = TreeNode.new(relatedTopic['name'])
+				
+				childNode.expand(level-1)
+				self.addChild(childNode)
+			end
+		end
+		self
+	end
+
+	def getRelatedTo(topic)
+		results = []
+		broadrequest = BroadFreebaseRequest.new(topic)
+		rawResponse = broadrequest.send().body
+		jsonResponse = JSON.parse(rawResponse)
+		results = jsonResponse['result']
+
+		narrowrequest = NarrowFreebaseRequest.new(topic)
+		rawResponse = narrowrequest.send().body
+		jsonResponse = JSON.parse(rawResponse)
+		jsonResponse['result'].each do |result|
+			results.push(result)
+		end
+		results
+	end
 end
